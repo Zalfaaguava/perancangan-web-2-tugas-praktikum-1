7e8e3120f15036939378bebafffbaa055c476d43
Zalfaaguava <zalfaaguava@gmail.com>
2025-10-01 11:22:04 +0700

    Create variabel.php

---
+++ latihan/variabel.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+        <title> Contoh Skrip PHP </title>
+</head>
+</body>
+     <?php
+     $nama = "Zalfaa Guava Rahma";
+     echo "$nama berkata \"Katakan namamu!\"";
+     ?>
+</body>
+</html>
